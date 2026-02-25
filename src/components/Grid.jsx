cdb75647c29315dfc1d34cf6dac960517ef3d199
Andre-tech671 <nyanjahiaandre@gmail.com>
2026-02-25 07:58:15 -0600

    feat: add WorkoutCard component to render training plan - Displays warmup and workout exercises with sets/reps - Includes help icons and weight input fields - Adds Save & Exit and Complete buttons

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -3,7 +3,7 @@ import { workoutProgram as training_plan } from '../utils/index.js'
 import WorkoutCard from './WorkoutCard.jsx'
 
 export default function Grid() {
-    const [savedWorkouts, setSavedWorkouts] = useState(null)
+    const [savedWorkouts, setSavedWorkouts] = useState({})
     const [selectedWorkout, setSelectedWorkout] = useState(null)
     const completedWorkouts = Object.keys(savedWorkouts || {}).filter((val) => {
         const entry = savedWorkouts[val]
